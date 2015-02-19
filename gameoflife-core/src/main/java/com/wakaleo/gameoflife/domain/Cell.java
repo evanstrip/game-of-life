dc6a30047a3720689fdaae5a13c7ee468135fac8
Hosung Hwang <ho-sung.hwang@senecacollege.ca>
2015-02-19 16:46:05 -0500

    some change

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,20 +1,15 @@
 package com.wakaleo.gameoflife.domain;
-
 public enum Cell {
-    LIVE_CELL("*"), DEAD_CELL(".");
-
+    LIVE_CELL("+"), DEAD_CELL(".");
     private String symbol;
-
-    private Cell(final String initialSymbol) {
-        this.symbol = initialSymbol;
+    private Cell(String symbol) {
+        this.symbol = symbol;
     }
-
     @Override
     public String toString() {
         return symbol;
     }
-
-    static Cell fromSymbol(final String symbol) {
+    static Cell fromSymbol(String symbol) {
         Cell cellRepresentedBySymbol = null;
         for (Cell cell : Cell.values()) {
             if (cell.symbol.equals(symbol)) {
@@ -24,8 +19,11 @@ public enum Cell {
         }
         return cellRepresentedBySymbol;
     }
-
-    public String getSymbol() {
-        return symbol;
-    }
 }
+
+
+public String getSymbol() {
+    return symbol;
+}
+
+
